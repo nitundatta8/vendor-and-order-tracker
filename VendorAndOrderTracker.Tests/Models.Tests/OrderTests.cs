091535a0c99ea6d8f5545f3236b363d733cc518e
nitunwa <nitun.2@gmail.com>
2020-05-22 11:22:17 -0700

    add OrderConstructor test case pass and GetProperty test fail

--- VendorAndOrderTracker.Tests/Models.Tests/OrderTests.cs
+++ VendorAndOrderTracker.Tests/Models.Tests/OrderTests.cs
@@ -10,8 +10,25 @@ namespace VendorAndOrderTracker.Tests
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Order newOrder = new Order("Bread", "White", "5-22-2020");
-      Assert.AreEqual(typeof(Vendor), newOrder.GetType());
+      Order newOrder = new Order("Bread", "White", 2, "5-22-2020");
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+    [TestMethod]
+    public void GetProperty_ReturnsProperty_Property()
+    {
+      String title = "Bread";
+      string description = "white";
+      int price = 2;
+      string date = "5-22-2020";
+      Order order = new Order(title, description, price, date);
+      string resultTitle = order.Title;
+      string resultDes = order.OrderDescription;
+      int resultPrice = order.Price;
+      string resultDate = order.Date;
+      Assert.AreEqual("title", resultTitle);
+      Assert.AreEqual("description", resultDes);
+      Assert.AreEqual(1, resultPrice);
+      Assert.AreEqual("date", resultDate);
     }
   }
 }
