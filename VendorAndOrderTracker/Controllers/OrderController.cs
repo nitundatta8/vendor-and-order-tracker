654931c21abbf0148016b516c229671336a79ff8
nitunwa <nitun.2@gmail.com>
2020-05-22 14:45:00 -0700

    add details to vendor show.cshtml view, New method to ordercontroller,details to the order

--- VendorAndOrderTracker/Controllers/OrderController.cs
+++ VendorAndOrderTracker/Controllers/OrderController.cs
@@ -4,5 +4,14 @@ using VendorAndOrderTracker.Models;
 
 namespace VendorAndOrderTracker.Controllers
 {
+  public class OrderController : Controller
+  {
+    [HttpGet("/vendors/{vendorId}/order/new")]
+    public ActionResult New(int vendorId)
+    {
+      Vendor vendor = Vendor.Find(vendorId);
+      return View(vendor);
+    }
 
+  }
 }
--- VendorAndOrderTracker/Controllers/VendorController.cs
+++ VendorAndOrderTracker/Controllers/VendorController.cs
@@ -17,5 +17,21 @@ namespace VendorAndOrderTracker.Controllers
     {
       return View();
     }
+    [HttpPost("/vendors")] // Creates categories
+    public ActionResult Create(string vendorName, string vendorDescription)
+    {
+      Vendor newVendor = new Vendor(vendorName, vendorDescription);
+      return RedirectToAction("Index");
+    }
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selecteVendor = Vendor.Find(id);
+      List<Order> vendorOrder = selecteVendor.Orders;
+      model.Add("vendor", selecteVendor);
+      model.Add("order", vendorOrder);
+      return View(model);
+    }
   }
 }
